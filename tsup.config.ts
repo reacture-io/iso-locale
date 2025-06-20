017580f5f6893637e6a5f2be528e7db65b65e3b1
Indy Van Canegem <i.vancanegem@faktion.com>
2025-06-20 02:39:02 +0200

    feat: better package support

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: {
     "countries/index": "src/countries/index.ts",
+    index: "src/index.ts",
   },
   outDir: "dist",
   format: ["esm", "cjs"],
